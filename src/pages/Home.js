597217076c6f2de7bca18b7b331c34f533b88020
Tu nombre <ejemplo@email.com>
2023-08-25 15:26:58 -0600

    25/08

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ function Home() {
     return (
         <div>
             <h1>Pokedex.lil</h1>
+            Hola
             <h4>Bienvenid@ a mi primera pagina wed echa 100% con React:</h4>
             <h5>La pagina wed se trata de Pokemones, Lo que hago es que estraigo las apis de los pokemones y las pinto en patalla y hago que sean aleatoreas cada que se reinicia la pagina wed.</h5>   
             <Pokemones/>   
