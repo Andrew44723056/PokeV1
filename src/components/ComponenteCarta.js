a009a24c022d55b14bee4ff01dcf9de73df31211
Tu nombre <ejemplo@email.com>
2023-09-15 22:13:06 -0600

    15/9

--- src/components/ComponenteCarta.js
+++ src/components/ComponenteCarta.js
@@ -4,16 +4,12 @@ import { Link } from "react-router-dom";
 import { postPokemonFavorite } from "./ComponenteLista";
 import { deletePokemonesFavorite } from "./ComponenteLista";
 
-
-
 function ComponenteCarta({ pokemonData }) {
-
-  const  [habilitar,sethabilitar] = useState(false);
+  const [habilitar, sethabilitar] = useState(false);
 
   async function favorites(id) {
-     await postPokemonFavorite(id);
-     sethabilitar(true);
- 
+    await postPokemonFavorite(id);
+    sethabilitar(true);
   }
 
   async function deletep(id) {
@@ -35,26 +31,30 @@ function ComponenteCarta({ pokemonData }) {
         )}
       </Link>
       {habilitar == false ? (
-    
-    <div onClick={() => favorites(pokemonData.id)} className="btn">
-    <span class="leftContainer">
-      <svg fill="white" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"></path></svg>
-      <span class="like">Like</span>
-    </span>
-    <span class="likeCount">
-      2,050
-    </span>
-  </div>
-        
-
-
+        <div onClick={() => favorites(pokemonData.id)} class="bookmarkBtn">
+          <span class="IconContainer">
+            <svg viewBox="0 0 384 512" height="0.9em" class="icon">
+              <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"></path>
+            </svg>
+          </span>
+          <p class="text">Save</p>
+        </div>
       ) : (
- 
-       
-<div onClick={() => deletep(pokemonData.id)} className="noselect"><span class="text">Delete</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></div>
-
-
-  
+        <div onClick={() => deletep(pokemonData.id)} class="btn">
+          <svg
+            viewBox="0 0 15 17.5"
+            height="17.5"
+            width="15"
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon2"
+          >
+            <path
+              transform="translate(-2.5 -1.25)"
+              d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z"
+              id="Fill"
+            ></path>
+          </svg>
+        </div>
       )}
     </div>
   );
